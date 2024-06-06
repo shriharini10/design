13061ba6a05bb04dbc789dc86d220d81abb7346e
shriharini10 <shri13anitha@gmail.com>
2024-06-06 17:02:19 +0530

    feat: adds search page to the flow

---
+++ app/Components/Search/Search.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Search.css'
+
+export const Search = () => {
+    return (
+        <div className='searchpage'>
+            <div className='searchtext'>
+                <p>Loading based on your search ....</p>
+            </div>
+
+            <div className='row1'>
+                <div className='grid1'>
+                    <img src={'/Search/search_image.svg'} alt="loading page" height={432} width={300} />
+                </div>
+
+                <div className='grid2'>
+                    <img src={'/Search/search_image.svg'} alt="loading page" height={432} width={300} />
+                </div>
+
+                <div className='grid3'>
+                    <img src={'/Search/search_image.svg'} alt="loading page" height={432} width={300} />
+                </div>
+
+                <div className='grid4'>
+                    <img src={'/Search/search_image.svg'} alt="loading page" height={432} width={300} />
+                </div>
+
+
+
+            </div>
+        </div>
+
+    );
+}
