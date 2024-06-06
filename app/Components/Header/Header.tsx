3b304cd614309721868951276145b3010dbfbbf5
shriharini10 <shri13anitha@gmail.com>
2024-06-06 22:50:13 +0530

    feat: imports necessary images for full auction list page

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,37 +1,49 @@
-import React from 'react'
+import React from 'react';
 import './Header.css'
+import Link from 'next/link';
 
 export const Header = () => {
-    return (
 
-        <div className='header'>
+  return (
 
-        <div className='brand'>
-          <img src={"/logo.svg"} alt={"MP4 NFT"} width={279} height={90} />
-        </div>
+    <div className='header'>
 
+      <div className='brand'>
+        <img src={"/logo.svg"} alt={"MP4 NFT"} width={279} height={90} />
+      </div>
+
+      
+      <div className='search'>
+        <img src={"/search.svg"} alt={"search image"} width={24} height={24} />
+        <input placeholder="Search Musicians, artists & more...." />
+      </div>
+      
+
+      {/* Use Link component to navigate to Search component */}
+      {/* <Link href="/Components/Search/Search" passHref>
         <div className='search'>
           <img src={"/search.svg"} alt={"search image"} width={24} height={24} />
           <input placeholder="Search Musicians, artists & more...." />
         </div>
+      </Link> */}
 
-        <div className='menu'>
-          <ul>
+      <div className='menu'>
+        <ul>
           <li><a>Home</a></li>
           <li><a>Create</a></li>
           <li><a>Collections</a></li>
-          </ul>
-        </div>
-
-        <div className='controls'>
-          <button>0 Collectibles</button>
-          <img src={"/Ellipse 5.svg"} alt={"user picture"} width={40} height={40} />
-        </div>
-
-        <div className='burger'>
-          <img src={"/hamburger.svg"} alt={"menu list"} width={24} height={24} />
-        </div>
-
+        </ul>
       </div>
-    );
+
+      <div className='controls'>
+        <button>0 Collectibles</button>
+        <img src={"/Ellipse 5.svg"} alt={"user picture"} width={40} height={40} />
+      </div>
+
+      <div className='burger'>
+        <img src={"/hamburger.svg"} alt={"menu list"} width={24} height={24} />
+      </div>
+
+    </div>
+  );
 }
