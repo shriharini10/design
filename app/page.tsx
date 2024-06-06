a69c58917243bc022608e4a8d5a89156e9910619
shriharini10 <shri13anitha@gmail.com>
2024-06-06 17:00:36 +0530

    feat: creates subsequent pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,21 @@ import { Header } from "./Components/Header/Header";
 //import styles from './page.module.css'
 import { Hero } from "./Components/Hero/Hero";
 import { Footer } from "./Components/Footer/Footer";
+import { Search } from "./Components/Search/Search";
+import { Create_preview } from "./Components/Create_preview/Create_preview";
+import { Create } from "./Components/Create/Create";
+
 
 export default function Home() {
   return (
     <>
     <Header/>
-    <Hero />
+    {/* <Hero /> */}
+    {/* <Search/> */}
+    {/* <Create_preview/> */}
+    <Create/>
     <Footer/>
+    
     </>
 
 
