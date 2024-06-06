9d8b2081c79cdd522ea839b0bff7274e07a5272b
shriharini10 <shri13anitha@gmail.com>
2024-06-06 17:01:54 +0530

    feat: adds create preview page

---
+++ app/Components/Create_preview/Create_preview.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Create_preview.css'
+
+export const Create_preview = () => {
+    return (
+        <div className='main'>
+            <div className='create'>
+                <h3>Create Music collectible</h3>
+
+                <div className='music_image'>
+                    <img src='/create/drop.svg' alt='create' height={230} width={500} />
+                </div>
+
+                <div className='sale'>
+                    <img src='/create/putonsale.svg' alt='create' />
+                </div>
+
+                <div className='price'>
+                    <img src='/create/sale.svg' alt='create' />
+                </div>
+
+                <div className='tax'>
+                    <img src='/create/tax.svg' alt='create'  />
+                </div>
+
+                <div className='name'>
+                    <img src='/create/name.svg' alt='create' />
+                </div>
+
+                <div className='description'>
+                    <img src='/create/desc.svg' alt='create' height={230} width={500} />
+                </div>
+
+                <div className='royalty'>
+                    <img src='/create/royalty.svg' alt='create'  />
+                </div>
+
+                <div className='create_button'>
+                    <img src='/create/button.svg' alt='create' />
+                </div>
+            </div>
+
+            <div className='preview'>
+                <h4>Preview</h4>
+
+                <div className='prev_image'>
+                    <img src={'/create/preview.svg'} alt='preview' height={400} width={500} />
+                </div>
+            </div>
+        </div>
+
+    );
+
+}
