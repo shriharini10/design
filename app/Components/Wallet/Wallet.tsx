b34cc6f76ee6a669750f66d6b74afe31f5499045
shriharini10 <shri13anitha@gmail.com>
2024-06-07 14:34:37 +0530

    feat: wallet page

---
+++ app/Components/Wallet/Wallet.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Wallet.css'
+
+export const Wallet = () => {
+    return (
+        <div>
+            <div className='foxy'>
+                <img src='/wallet/fox.svg' alt='fox image'/>
+                <p>Connect your Metamask Wallet to continue</p>
+                <button>Connect Wallet</button>
+            </div>
+
+            
+
+        </div>
+    );
+}
